e9251420f88e9b555fca04e53b1c0e6f58083752
uvsl <ugovslima@gmail.com>
2025-01-15 21:06:45 -0300

    users and orders services, repositories and resources created

---
+++ FirstSpringProject/src/main/java/com/studying/FirstSpringProject/entities/User.java
@@ -0,0 +1,97 @@
+package com.studying.FirstSpringProject.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    @Serial
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User () {
+    }
+
+    public User(Integer id, String password, String phone, String email, String name) {
+        this.id = id;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
---
+++ FirstSpringProject/src/main/java/com/studying/FirstSpringProject/resources/OrderResource.java
@@ -0,0 +1,32 @@
+package com.studying.FirstSpringProject.resources;
+
+import com.studying.FirstSpringProject.entities.Order;
+import com.studying.FirstSpringProject.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Integer id) {
+        Order obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
